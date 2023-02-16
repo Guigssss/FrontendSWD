87473cfe39ca759bada2d21bf67eefde2fb41baa
Guigssss <guillaume.mouty@gmail.com>
2023-02-16 08:59:44 +0100

    modifying config

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,17 +1,7 @@
-import adapter from '@sveltejs/adapter-auto';
-import vercel from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-vercel';
 
-import { vitePreprocess } from '@sveltejs/kit/vite';
-
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-
+export default {
 	kit: {
-		adapter: vercel()
+		adapter: adapter()
 	}
 };
-
-export default config;
